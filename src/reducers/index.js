d7af71e3c6c849d813da56953e4496e8cd349154
taratimmerman <timmermantara@gmail.com>
2021-07-28 21:46:56 -0600

    connected actions to reducers -- and reducers to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
-import { combineReducers } from "redux";
-import auth from "./auth";
-import message from "./message";
+import { combineReducers } from 'redux';
 
-export default combineReducers({
-  auth,
-  message,
+import { authentication } from './authentication.reducer';
+import { registration } from './registration.reducer';
+import { users } from './users.reducer';
+import { alert } from './alert.reducer';
+
+const rootReducer = combineReducers({
+  authentication,
+  registration,
+  users,
+  alert
 });
+
+export default rootReducer;
