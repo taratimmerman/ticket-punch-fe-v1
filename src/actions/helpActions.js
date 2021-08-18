ed0d8bc479fa493b512586f761a83911def90d6a
taratimmerman <timmermantara@gmail.com>
2021-08-18 11:57:44 -0600

    Created help modal action and reducer

---
+++ src/actions/helpActions.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+import { closeHelpModalAction } from '../actions/modalActions';
+
+const webHookURL = 'https://hooks.slack.com/services/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';
+
+// HELP ACTION TYPES
+export const MSG_DEV_REQUEST = "HELP_MSG_DEV_REQUEST";
+export const MSG_DEV_SUCCESS = "HELP_MSG_DEV_SUCCESS";
+export const MSG_DEV_FAILURE = "HELP_MSG_DEV_REQUEST";
+
+// HELP ACTION CREATORS
+export const msgDevAction = (name, email, question) => dispatch => {
+    dispatch({ type: MSG_DEV_REQUEST });
+
+    const data = {
+        'text': `NAME: ${name}\n EMAIL: ${email}\n MESSAGE: ${question}`
+    };
+
+    axios
+        .post(webHookURL, JSON.stringify(data), {
+            withCredentials: false,
+            transformRequest: [(data, headers) => {
+                delete headers.post['Content-Type'];
+                return data;
+            }]
+        })
+        .then(res => {
+            dispatch({
+                type: MSG_DEV_SUCCESS,
+                payload: res.data
+            });
+            setTimeout(() => {
+                closeHelpModalAction();
+            }, 15000);
+        })
+        .catch(err => {
+            dispatch({
+                type: MSG_DEV_FAILURE,
+                payload: err.message
+            });
+        });
+};
---
+++ src/reducers/helpReducer.js
@@ -0,0 +1,37 @@
+import {
+    MSG_DEV_REQUEST,
+    MSG_DEV_SUCCESS,
+    MSG_DEV_FAILURE
+} from '../actions/helpActions';
+
+// INITIAL HELP STATE
+const initialMsgDevState = {
+    status: '',
+    success: null,
+    error: null
+};
+
+// HELP REDUCER
+export const helpReducer = (state = initialMsgDevState, action) => {
+    switch (action.type) {
+        case MSG_DEV_REQUEST:
+            return {
+                ...state,
+                status: 'Pending...'
+            };
+        case MSG_DEV_SUCCESS:
+            return {
+                ...state,
+                success: action.payload,
+                status: 'Success'
+            };
+        case MSG_DEV_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                status: 'Failed'
+            };
+        default:
+            return state;
+    }
+};
