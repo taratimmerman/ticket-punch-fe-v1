4e10cdb00c9d50ca8b0e54b2d5d7abc15906863d
taratimmerman <timmermantara@gmail.com>
2021-08-27 12:20:24 -0600

    Put help modal in its own component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-import { useForm } from 'react-hook-form';
 import { GiBoxingGlove } from 'react-icons/gi';
 import { IoTicketOutline, IoHelpCircleOutline } from 'react-icons/io5';
 import { VscHistory, VscAccount } from 'react-icons/vsc';
@@ -10,74 +9,10 @@ import { NavLink } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
 
-import { msgDevAction } from '../actions/helpActions';
-import { openHelpModalAction, closeHelpModalAction } from '../actions/modalActions';
-import SuccessMessage from '../components/SuccessMessage';
-import {
-    ModalContainer,
-    ModalCircle,
-    ModalAction,
-    ModalDetails,
-    ModalButtonContainer
-} from '../styling/ModalStyling';
-import {
-    SolidInput,
-    SolidTextArea,
-    StyledForm,
-    StyledLabel
-} from '../styling/PageStyling';
-import Button from './button/Button';
-import ErrorMessage from './errors/ErrorMessage';
-import InlineErrorMessage from './errors/InlineErrorMessage';
+import { openHelpModalAction } from '../actions/modalActions';
+import HelpModal from './modals/HelpModal';
 
-const Navbar = ({ openHelpModalAction, closeHelpModalAction, showHelpModal, msgDevAction }) => {
-
-    const { register, handleSubmit, reset, formState: { errors } } = useForm({
-        mode: "onBlur"
-    });
-
-    let errorMessage;
-    let successMessage;
-
-    const handleMsgDev = (message) => {
-        console.log(message);
-        const name = message.name.trim();
-        const email = message.email.trim();
-        const question = message.question.trim();
-
-        msgDevAction(name, email, question);
-        reset();
-    };
-
-    const handleError = (errors) => console.log(errors);
-
-    const msgDevValidation = {
-        name: {
-            required: "Please enter your name",
-            maxLength: {
-                value: 140,
-                message: "Please adjust your name to be less than 140 characters"
-            },
-            minLength: {
-                value: 2,
-                message: "Name input must be at least 2 characters"
-            }
-        },
-        email: {
-            required: "Please enter your email address",
-            pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Enter in the format: name@company.com"
-            }
-        },
-        question: {
-            required: "Please enter your question",
-            maxLength: {
-                value: 300,
-                message: "Please adjust your question to be less than 300 characters"
-            }
-        }
-    };
+const Navbar = ({ openHelpModalAction, showHelpModal}) => {
 
     return (
         <NavContainer>
@@ -105,89 +40,7 @@ const Navbar = ({ openHelpModalAction, closeHelpModalAction, showHelpModal, msgD
                     <LinkTitle>Help</LinkTitle>
                 </HelpIcon>
 
-                {/* HELP MODAL */}
-                < ModalContainer
-                    className="yellow"
-                    isOpen={showHelpModal}
-                    onRequestClose={() => closeHelpModalAction()}
-                    closeTimeoutMS={200}
-                    contentLabel="modal"
-                >
-                    <ModalCircle className="yellow">
-                        <IoHelpCircleOutline />
-                    </ModalCircle>
-                    <ModalAction>Questions?</ModalAction>
-                    <ModalDetails>Contact the dev!</ModalDetails>
-
-                    <ErrorMessage error={errorMessage} />
-                    <SuccessMessage success={successMessage}/>
-
-                    <StyledForm onSubmit={handleSubmit(handleMsgDev, handleError)}>
-                        <StyledLabel
-                            htmlFor="name"
-                        >
-                            Name
-                        </StyledLabel>
-                        <SolidInput
-                            type="text"
-                            {...register("name", msgDevValidation.name)}
-                            name="name"
-                            className={`yellow ${errors.name ? "error" : null}`}
-                            placeholder="Enter your name"
-                        />
-                        {errors.name ?
-                            <InlineErrorMessage inlineErrorMessage={errors.name.message} />
-                            : null}
-
-                        <StyledLabel
-                            htmlFor="email"
-                        >
-                            Email
-                        </StyledLabel>
-                        <SolidInput
-                            type="email"
-                            {...register("email", msgDevValidation.email)}
-                            email="email"
-                            className={`yellow ${errors.email ? "error" : null}`}
-                            placeholder="Enter your email"
-                        />
-                        {errors.email ?
-                            <InlineErrorMessage inlineErrorMessage={errors.email.message} />
-                            : null}
-
-                        <StyledLabel
-                            htmlFor="question"
-                        >Question/Message</StyledLabel>
-
-                        <SolidTextArea
-                            type="text"
-                            {...register('question', msgDevValidation.question)}
-                            name="question"
-                            className={`yellow ${errors.question ? "error" : null}`}
-                            placeholder="Enter your question/message"
-                        />
-                        {errors.question ?
-                            <InlineErrorMessage inlineErrorMessage={errors.question.message}/>
-                            : null}
-
-                        <ModalButtonContainer>
-                            <Button
-                                type="submit"
-                                className="yellow"
-                                text={"Email Tara"}
-                            />
-                        </ModalButtonContainer>
-                    </StyledForm>
-
-                    <ModalButtonContainer>
-                        <Button
-                        className="yellow secondary"
-                        onClick={() => closeHelpModalAction()}
-                        text={"Cancel"}
-                        />
-                    </ModalButtonContainer>
-
-                </ModalContainer >
+                {showHelpModal ? <HelpModal /> : null}
 
                 <StyledNavLink to="/profile">
                     <VscAccount />
@@ -201,9 +54,7 @@ const Navbar = ({ openHelpModalAction, closeHelpModalAction, showHelpModal, msgD
 
 Navbar.propTypes = {
     openHelpModalAction: PropTypes.func,
-    closeHelpModalAction: PropTypes.func,
-    showHelpModal: PropTypes.bool,
-    msgDevAction: PropTypes.func
+    showHelpModal: PropTypes.bool
 };
 
 const mapStateToProps = (state) => {
@@ -214,9 +65,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
-        openHelpModalAction: openHelpModalAction,
-        closeHelpModalAction: closeHelpModalAction,
-        msgDevAction: msgDevAction
+        openHelpModalAction: openHelpModalAction
     }, dispatch);
 };
 
